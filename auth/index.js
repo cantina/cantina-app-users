df01a3fe7b9df996076c916326f37cf398528dc8
erinlesavoy <erin.lesavoy@gmail.com>
2014-03-26 10:27:21 -0400

    Adds basic auth plugin

---
+++ auth/index.js
@@ -0,0 +1,47 @@
+var app = require('cantina')
+  , async = require('async');
+
+app.serializeUser = function (user, cb) {
+  cb(null, user.id);
+};
+
+app.deserializeUser = function (id, cb) {
+  app.users.load(id, cb);
+};
+
+require('cantina-redis');
+require('cantina-session');
+require('cantina-auth');
+
+app.auth = {};
+
+app.auth.logIn = function (user, req, res, next) {
+  req.logIn(user, function (err){
+    if (err) return next(err);
+    app.redis.SADD(app.redisKey('sessions', user.id), req.sessionID, next);
+  });
+};
+
+app.auth.killSession = function (user, cb) {
+  var key = app.redisKey('sessions', user.id ? user.id : user);
+  app.redis.SMEMBERS(key, function (err, members) {
+    if (err) return cb(err);
+    if (members) {
+      async.forEach(members, app.sessions.destroy, function (err) {
+        if (err) return cb(err);
+        app.redis.DEL(key, cb);
+      });
+    }
+    else cb();
+  });
+};
+
+app.auth.logOut = function (req, cb) {
+  var user = req.user;
+  if (!user) {
+    app.log.warn('Logout: no user');
+    return cb();
+  }
+  req.logOut();
+  app.auth.killSession(user, cb);
+};

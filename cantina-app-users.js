5262f50ed8ff7b0dbe6bf4fa63bedd2fd0f56fe1
Dan MacTough <danmactough@gmail.com>
2014-04-04 14:47:48 -0400

    Add cantina-models-schemas and default user schema

--- cantina-app-users.js
+++ cantina-app-users.js
@@ -1,8 +1,21 @@
-var app = require('cantina');
+var app = require('cantina')
+  , defaultUserSchema = require('./default-user-schema');
 
 require('cantina-models-mongo');
+require('cantina-models-schemas');
+
+app.users = {
+  // The app can override or extend the schema
+  schema: defaultUserSchema
+};
+
+app.once('collection:create:users', function (collection) {
+  collection.ensureIndex(app.users.schema._indexes, function (err) {
+    if (err) app.emit('error', err);
+  });
+});
 
 app.hook('start').add(function (done) {
-  app.createMongoCollection('users');
+  app.createMongoCollection('users', app.schemas.getCollectionOptions(app.users.schema));
   done();
 });

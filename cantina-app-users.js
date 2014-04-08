176d55e3b1d1d47eb516ef525d3f06c62ae9c056
Dan MacTough <danmactough@gmail.com>
2014-04-08 11:31:49 -0400

    Update to pass ensureIndex as init option

--- cantina-app-users.js
+++ cantina-app-users.js
@@ -5,13 +5,13 @@ require('cantina-models-schemas');
 
 app.loadSchemas('schemas', __dirname);
 
-app.once('collection:create:users', function (collection) {
-  collection.ensureIndex(app.schemas.user.indexes.mongo, function (err) {
-    if (err) app.emit('error', err);
-  });
-});
-
 app.hook('start').add(function (done) {
-  app.createMongoCollection('users', app.schemas.user.getOptions());
+  app.createMongoCollection('users', app.schemas.user.getOptions({
+    init: function (collection) {
+      collection.ensureIndex(app.schemas.user.indexes.mongo, function (err) {
+        if (err) app.emit('error', err);
+      });
+    }
+  }));
   done();
 });

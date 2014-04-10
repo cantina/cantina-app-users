8ce9ffa1dad10efa69394f22311bd1f16854d503
erinlesavoy <erin.lesavoy@gmail.com>
2014-04-10 18:06:10 -0400

    Kill user's sessions when status becomes invalid

--- auth/index.js
+++ auth/index.js
@@ -51,3 +51,12 @@ app.auth.logOut = function (req, cb) {
 app.hook('model:afterDestroy:users').add(function (user, done) {
   app.auth.killSession(user, done);
 });
+
+app.hook('model:afterSave:users').add(function (user, done) {
+  if (app.conf.get('app:users:authenticate:status').indexOf(user.status) < 0) {
+    app.auth.killSession(user, done);
+  }
+  else {
+    done();
+  }
+});

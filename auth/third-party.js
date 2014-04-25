62eed5d31e3981d2d421143c24d8cc5d0776a92e
Dan MacTough <danmactough@gmail.com>
2014-04-25 12:32:45 -0400

    Fix missing dependency. Also only define 3rd party helpers if needed.

--- auth/third-party.js
+++ auth/third-party.js
@@ -1,4 +1,5 @@
-var app = require('cantina');
+var app = require('cantina')
+  , _ = require('underscore');
 
 if (app.conf.get('auth-twitter') || app.conf.get('auth-facebook')) {
   app.Schema.extend(app.schemas.user, {
@@ -13,6 +14,46 @@ if (app.conf.get('auth-twitter') || app.conf.get('auth-facebook')) {
       }
     }
   });
+}
+
+if (app.conf.get('auth-twitter')) {
+  app.verifyTwitterUser = function (token, tokenSecret, profile, done) {
+
+    var nameParts = profile._json.name.split(' ');
+    var normalizedData = {
+      provider: {
+        id: profile.id,
+        name: profile.provider
+      },
+      username: profile.username || profile.displayName,
+      name: {
+        first: nameParts.length ? nameParts[0] : undefined,
+        last: nameParts.length > 1 ? nameParts[1] : undefined,
+        full: profile.__json.name
+      },
+      email: profile._json.email,
+      avatar: profile.photos && profile.photos.length ? profile.photos[0].value : undefined
+    };
+    return createOrUpdateProfile(normalizedData, done);
+  };
+}
+
+if (app.conf.get('auth-facebook')) {
+  app.verifyFacebookUser = function (token, tokenSecret, profile, done) {
+
+    var normalizedData = {
+      provider_id: profile.id,
+      provider: profile.provider,
+      username: profile.username || profile.displayName,
+      name: {
+        first: profile.__json.first_name,
+        last: profile.__json.last_name
+      },
+      email: profile._json.email,
+      avatar: 'https://graph.facebook.com/' + profile.id + '/picture'
+    };
+    return createOrUpdateProfile(normalizedData, done);
+  };
 }
 
 function createOrUpdateProfile (data, done) {
@@ -37,39 +78,3 @@ function createOrUpdateProfile (data, done) {
     done(null, false, {message: 'Not a valid user'});
   }
 }
-
-app.verifyTwitterUser = function (token, tokenSecret, profile, done) {
-
-  var nameParts = profile._json.name.split(' ');
-  var normalizedData = {
-    provider: {
-      id: profile.id,
-      name: profile.provider
-    },
-    username: profile.username || profile.displayName,
-    name: {
-      first: nameParts.length ? nameParts[0] : undefined,
-      last: nameParts.length > 1 ? nameParts[1] : undefined,
-      full: profile.__json.name
-    },
-    email: profile._json.email,
-    avatar: profile.photos && profile.photos.length ? profile.photos[0].value : undefined
-  };
-  return createOrUpdateProfile(normalizedData, done);
-};
-
-app.verifyFacebookUser = function (token, tokenSecret, profile, done) {
-
-  var normalizedData = {
-    provider_id: profile.id,
-    provider: profile.provider,
-    username: profile.username || profile.displayName,
-    name: {
-      first: profile.__json.first_name,
-      last: profile.__json.last_name
-    },
-    email: profile._json.email,
-    avatar: 'https://graph.facebook.com/' + profile.id + '/picture'
-  };
-  return createOrUpdateProfile(normalizedData, done);
-};
